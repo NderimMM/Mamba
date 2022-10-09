50a88c77582e5a243e886131772275eb07589e0d
nderimmM <nderim.murati321@gmail.com>
2022-10-09 16:17:36 +0200

    krijimi i applikacionit react frontent si dhe krijimi i te dhenave nga te dhena statike ne te dhena dinamike

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:'Samsung Galaxy S22 Ultra',
+            category:'Mobile',
+            image:'/images/p1.jpg',
+            price:'1200',
+            brand:'Samsung',
+            rating:5,
+            numReviews:10,
+            description:'High quality product'
+        },
+        {
+            _id:'2',
+            name:'Iphone 13 ProMAx',
+            category:'Mobile',
+            image:'/images/p2.jpg',
+            price:'1200',
+            brand:'Apple',
+            rating:5,
+            numReviews:10,
+            description:'High quality product'
+        },
+        {
+            _id:'3',
+            name:'Redmi Note 20',
+            category:'Mobile',
+            image:'/images/p3.jpg',
+            price:'1200',
+            brand:'Redmi',
+            rating:5,
+            numReviews:10,
+            description:'High quality product'
+        },
+        {
+            _id:'4',
+            name:'Mac Book Pro',
+            category:'Laptop',
+            image:'/images/p4.png',
+            price:'1200',
+            brand:'Apple',
+            rating:5,
+            numReviews:10,
+            description:'High quality product'
+        },
+        {
+            _id:'5',
+            name:'Nvidia Graphic RTX 3090',
+            category:'Eletronic',
+            image:'/images/p5.png',
+            price:'1200',
+            brand:'Nvidia',
+            rating:5,
+            numReviews:10,
+            description:'High quality product'
+        },
+        {
+            _id:'6',
+            name:'Nvidia Graphic GTX 970',
+            category:'Eletronic',
+            image:'/images/p6.jpg',
+            price:'1200',
+            brand:'Nvidia',
+            rating:4.5,
+            numReviews:10,
+            description:'High quality product'
+        },
+    ],
+}
+
+export default data;
